a62533b30f28c601b3e182cad34a19e56657aa54
phuongnk27 <phuongnk2709@gmail.com>
2023-07-06 18:22:22 -0600

    Update app.py
    Display summoner stats and champion pool

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 import streamlit as st
 import json
-import requests
+from data import get_summary, region_list, get_champion_pool
+import matplotlib.pyplot as plt
+import pandas as pd
 
-
-
-region_list = {
-    "NA": "North America", "EUW": "Europe West",
-    "EUN": "Europe Nordic & East", "KR": "Korea",
-    "BR": "Brazil", "JP": "Japan",
-    "RU": "Russia", "OCE": "Oceania",
-    "TR": "Turkey", "LAN": "Latin America North",
-    "LAS": "Latin America South", "PH": "Philippines",
-    "SG": "Singapore", "TH":"Thailand",
-    "TW": "Taiwan", "VN": "Vietnam"
-}
 
 def display_champion_images(champions, champion_data):
     row = ""
@@ -24,13 +14,63 @@ def display_champion_images(champions, champion_data):
     if row != "":
         st.markdown(row, unsafe_allow_html=True)
 
-def summoner_info():
-    st.subheader("Summary")
+def summoner_info(summoner_name, region):
 
+    stats = get_summary(summoner_name, region)
+    if stats:
+        # Display summoner info
+        name = summoner_name.replace('%20', ' ')
+        st.subheader(f"{name}")  
+        tier = stats['tier'].lower()
+        tier = tier[0].upper() + tier[1:]
+        rank = f"{tier} {stats['rank']}"
+        lp = stats['leaguePoints']
+        st.write(f"__{rank}__ - {lp} LP")
+
+        col1, col2 = st.columns([2,7])
+    
+        with col1:
+            # Calculate win rate percentage
+            wins = stats['wins']
+            losses = stats['losses']
+            total_games = wins + losses
+            win_rate = (wins / total_games) * 100
+
+            # Display plot
+            # Create a circle at the center of the plot
+            my_circle = plt.Circle((0, 0), 0.7, color="#0E1117")
+            # Create the pie chart
+            fig, ax = plt.subplots(figsize=(6,4),facecolor="#0E1117")
+            ax.pie([wins, losses],colors=['skyblue', 'red'])
+
+            # Add the title 
+            caption = f"{total_games}G {wins}W {losses}L"
+            ax.set_title(caption, loc='center', fontsize=20, color="#FAFAFA")
+
+            # Add the win rate text in the middle of the pie chart
+            ax.text(0, 0, f"{int(win_rate)}%", horizontalalignment='center', verticalalignment='center', fontsize=20,
+                    color="#FAFAFA")
+            
+            # Add the circle to the plot
+            p = plt.gcf()
+            p.gca().add_patch(my_circle)
+
+            # Show the graph in Streamlit
+            st.pyplot(fig)
+
+        # Display champion pool
+        with col2:
+            df = get_champion_pool(summoner_name, region)
+            st.dataframe(df)
+    
+    # No data available
+    else: 
+        st.subheader('Sorry, no ranked data available')
+
+    
 def champion_draft():
     st.subheader("Champion Draft Pick")
     # Fetch champion data
-    # Opening JSON file
     with open('champion.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     
@@ -68,21 +108,22 @@ def champion_draft():
         st.write("- Champion 3")
         # Add more recommendations as needed
 
-
 def main():
     st.title("League of Legends Champion Recommendation")
     
     # Summoner Name
     summoner_name = st.text_input("Enter Summoner Name:")
+    summoner_name = summoner_name.replace(" ", "%20")
     
     # Regions Dropdown
     regions = region_list.keys()  # Add more regions as needed
     selected_region = st.selectbox("Select Region:", regions)
+ 
     
     if summoner_name:
         tab1, tab2= st.tabs(["Summary", "Champion Draft"])     
         with tab1:
-            summoner_info()
+            summoner_info(summoner_name, selected_region)
 
         with tab2:
             champion_draft()
